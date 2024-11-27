b0387f4e3ea3bad7f4ff0d29a7cb4bbec92cb928
John Kideys <johnkideys@gmail.com>
2024-11-27 21:10:56 +0000

    Replac the sentiment_analysis_task function to scripts

--- airflow_home/dags/nyt_pipeline_dag_1.py
+++ airflow_home/dags/nyt_pipeline_dag_1.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from scripts.tasks import get_articles, upload_to_gcs, read_from_gcs_and_load_duckdb
-from scripts.sentiment_analysis import analyse_sentiment
+from scripts.sentiment_analysis import sentiment_analysis_task
 import duckdb
 
 # Define default args for the DAG
@@ -39,34 +39,7 @@ with DAG(
     load_to_duckdb_task = PythonOperator(
         task_id='load_to_duckdb',
         python_callable=read_from_gcs_and_load_duckdb,
-    )
-
-    # Task 4: Perform sentiment analysis
-    def sentiment_analysis_task():
-        # Connect to DuckDB
-        conn = duckdb.connect('nyt_warehouse.db')
-
-        # Fetch article headlines
-        query = "SELECT article_id, headline FROM nyt_articles;"
-        df = conn.execute(query).fetchdf()
-
-        # Map sentiment categories
-        def map_sentiment(sentiment):
-            return {0: "Negative", 1: "Neutral", 2: "Positive"}[sentiment]
-
-        # Apply sentiment analysis
-        df['sentiment'] = df['headline'].apply(lambda x: map_sentiment(analyse_sentiment(x)))
-
-        # Update database with sentiment
-        for _, row in df.iterrows():
-            conn.execute("""
-                UPDATE nyt_articles
-                SET sentiment = ?
-                WHERE article_id = ?;
-            """, [row['sentiment'], row['article_id']])
-
-        # Commit and close connection
-        conn.close()
+    )   
 
     sentiment_task = PythonOperator(
         task_id='perform_sentiment_analysis',
--- airflow_home/dags/scripts/sentiment_analysis.py
+++ airflow_home/dags/scripts/sentiment_analysis.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+import duckdb
 
 # Load the model and tokenizer
 model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
@@ -13,3 +14,29 @@ def analyse_sentiment(text):
     scores = torch.nn.functional.softmax(outputs.logits, dim=-1)
     sentiment = torch.argmax(scores).item()  # 0: Negative, 1: Neutral, 2: Positive
     return sentiment
+
+# Task 4: Perform sentiment analysis
+def sentiment_analysis_task():
+    
+    # Connect to DuckDB
+    conn = duckdb.connect('nyt_warehouse.db')
+    # Fetch article headlines
+    query = "SELECT article_id, headline FROM nyt_articles;"
+    df = conn.execute(query).fetchdf()
+
+    # Map sentiment categories
+    def map_sentiment(sentiment):
+        return {0: "Negative", 1: "Neutral", 2: "Positive"}[sentiment]
+
+    # Apply sentiment analysis
+    df['sentiment'] = df['headline'].apply(lambda x: map_sentiment(analyse_sentiment(x)))
+
+    # Update database with sentiment
+    for _, row in df.iterrows():
+        conn.execute("""
+            UPDATE nyt_articles
+            SET sentiment = ?
+            WHERE article_id = ?;
+        """, [row['sentiment'], row['article_id']])
+    # Commit and close connection
+    conn.close()
